eedb851c230f669be9559944de4abf54f3d4e7eb
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-04-24 15:59:18 +0800

    update all file name space

--- tests.ts
+++ tests.ts
@@ -1,36 +1,36 @@
-WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
+smarthonIoTBit.on_wifi_connect(function (IP_Address, Device_ID) {
     basic.showString(Device_ID)
 })
-WiFiIoT.on_IFTTT_conn(function (Status, Error_code) {
+smarthonIoTBit.on_IFTTT_conn(function (Status, Error_code) {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.A, function () {
-    if (WiFiIoT.is_wifi_connect()) {
-        WiFiIoT.sendThingspeak(
+    if (smarthonIoTBit.is_wifi_connect()) {
+        smarthonIoTBit.sendThingspeak(
         "CQZ89FEYBJ9CAQ9M",
         1
         )
     }
 })
-WiFiIoT.on_NTP_Received(function (Year, Month, Day, Hour, Minute, Second) {
+smarthonIoTBit.smarthon_iot_bit_on_NTP_Received(function (Year, Month, Day, Hour, Minute, Second) {
     basic.showString("" + Year + "," + Month + "," + Day + "|" + Hour + ":" + Minute + ":" + Second)
 })
-WiFiIoT.on_WAN_remote(function (WAN_Command) {
+smarthonIoTBit.smarthon_iot_bit_on_WAN_remote(function (WAN_Command) {
     basic.showString(WAN_Command)
 })
-WiFiIoT.on_thingspeak_conn(function (Status, Error_code) {
+smarthonIoTBit.smarthon_iot_bit_on_thingspeak_conn(function (Status, Error_code) {
     basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.AB, function () {
-    WiFiIoT.getNTP(WiFiIoT.CityList.HongKong)
+    smarthonIoTBit.smarthon_iot_bit_getNTP(smarthonIoTBit.CityList.HongKong)
 })
 input.onButtonPressed(Button.B, function () {
-    if (WiFiIoT.is_wifi_connect()) {
-        WiFiIoT.sendIFTTT(
+    if (smarthonIoTBit.smarthon_iot_bit_is_wifi_connect()) {
+        smarthonIoTBit.smarthon_iot_bit_sendIFTTT(
         "cEiZt5Okq11jhgIRlYm6pF",
         "hi"
         )
     }
 })
-WiFiIoT.initializeWifi(SerialPin.P16, SerialPin.P8)
-WiFiIoT.setWifi("smarthon", "12345678")
+smarthonIoTBit.smarthon_iot_bit_initializeWifi(SerialPin.P16, SerialPin.P8)
+smarthonIoTBit.smarthon_iot_bit_setWifi("smarthon", "12345678")
